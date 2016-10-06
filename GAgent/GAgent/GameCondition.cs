6c473976db767d27565a25b857d73b84a25c4e8b
samirawad <samir.awad@gmail.com>
2016-10-06 09:24:50 -0400

    Conditions work
    Wrapped the selectors and operators in classes so its easier to log
    them.
    Todo: Add additional data to currentAction and currentOutcome so that
    they can be accessed by conditions.

--- GAgent/GAgent/GameCondition.cs
+++ GAgent/GAgent/GameCondition.cs
@@ -20,24 +20,73 @@ namespace GAgent
      * Hopefully with this structure we can create nested condition which are easy to log.
      */
 
+    public class ConditionOperator
+    {
+        private string ID;
+        private string Description;
+        private ConditionOperatorDelegate Operator;
+
+        public ConditionOperator(string inId, string inDesc, ConditionOperatorDelegate inOperator)
+        {
+            ID = inId;
+            Description = inDesc;
+            Operator = inOperator;
+        }
+
+        public bool IsValid(List<Condition> conditions, GameWorld world)
+        {
+            bool result = false;
+            Console.WriteLine(Description);
+            result = Operator(conditions, world);
+            Console.WriteLine(result == true ? " - success!" : " - failure!");
+            return result;
+        }
+    }
+
+    public class AgentSelector
+    {
+        private string ID;
+        private string Description;
+        private AgentSelectorDelegate Selector;
+
+        public AgentSelector(string inId, string inDesc, AgentSelectorDelegate inSelector)
+        {
+            ID = inId;
+            Description = inDesc;
+            Selector = inSelector;
+        }
+
+        public Dictionary<string, GameAgent> GetAgents(GameWorld world)
+        {
+            Dictionary<string, GameAgent> result = null;
+            Console.WriteLine(Description);
+            result = Selector(world);
+            Console.WriteLine(result != null ? " - success!" : " - failure!");
+            return result;
+        }
+    }
+
     public class Condition
     {
-        private ConditionOperatorDelegate Operation;
+        private ConditionOperator Operation;
         private ConditonDelegate ValidCondition;
-        private AgentSelectorDelegate Selector;
+        private AgentSelector Selector;
         private List<Condition> Conditions;
         private string Description;
+        private string ID;
 
-        public Condition(string inDescription, AgentSelectorDelegate inSelector, ConditonDelegate inDel)
+        public Condition(string id, string inDescription, AgentSelector inSelector, ConditonDelegate inDel)
         {
+            ID = id;
             Description = inDescription;
             ValidCondition = inDel;
             Selector = inSelector;
             Conditions = null;
         }
 
-        public Condition(string inDescription, ConditionOperatorDelegate inOperation, List<Condition> inConditions)
+        public Condition(string id, string inDescription, ConditionOperator inOperation, List<Condition> inConditions)
         {
+            ID = id;
             Description = inDescription;
             Conditions = inConditions;
             Operation = inOperation;
@@ -49,20 +98,15 @@ namespace GAgent
             // The validity of a condtion is either the validcondtion, or the operation on the subconditions.
             Console.Write("Resolving condition: " + Description);
             bool result;
-            if(ValidCondition != null)
+            if (ValidCondition != null)
             {
-                result = ValidCondition(Selector(w), w);
+                result = ValidCondition(Selector.GetAgents(w), w);
             }
             else
             {
-                result = Operation(Conditions, w);
+                result = Operation.IsValid(Conditions, w);
             }
-            
-            if (result == true)
-                Console.WriteLine(" - success!");
-            else
-                Console.WriteLine(" - failed!");
-
+            Console.WriteLine(result == true ? " - success! " : " - failure! ");
             return result;
         }
     }
@@ -76,47 +120,56 @@ namespace GAgent
     {
         public ConditionTester()
         {
-            ConditionOperatorDelegate andOperator = (conditions, world) =>
-            {
-                // for the 'and' operator, all conditions must be valid
-                return conditions.All(c => c.IsValid(world));
-            };
+            ConditionOperator andOperator = new ConditionOperator("and", "All conditions must be valid",
+                (conditions, world) =>
+                {
+                    // for the 'and' operator, all conditions must be valid
+                    return conditions.All(c => c.IsValid(world));
+                });
 
-            AgentSelectorDelegate playerSelector = (world) =>
-            {
-                // we could place logging in here so we can see if selectors fail
-                Console.WriteLine("Selecting player.");
-                return new Dictionary<string, GameAgent>(){
-                        {"player", world.AllEntities["player"]}
-                    };
-            };
+            AgentSelector playerSelector = new AgentSelector("playerSelector", "Selects the player from the game world", 
+                (world) =>
+                {
+                    // we could place logging in here so we can see if selectors fail
+                    Console.WriteLine("Selecting player.");
+                    return new Dictionary<string, GameAgent>(){
+                            {"player", world.AllEntities["player"]}
+                        };
+                });
+            
 
-            Condition atRest = new Condition("The player is at rest", 
+            Condition atRest = new Condition("atrest", "The player is at rest",
                 playerSelector,
                 (selection, world) =>
                 {
                     return selection["player"].S["CurrentAction"] == "resting" ? true : false;
                 });
-            Condition validDestination = new Condition("The desination is valid", 
+            Condition validDestination = new Condition("validdest", "The desination is valid",
                 playerSelector,
                 (selection, world) =>
                 {
                     return selection["player"].S["Location"] != selection["player"].S["Destination"] ? true : false;
                 });
-            Condition notTravelling = new Condition("The player is not currently travelling",
+            Condition notTravelling = new Condition("notTravelling", "The player is not currently travelling",
                 playerSelector,
                 (selection, world) =>
                 {
                     return selection["player"].S["Destination"] == null ? true : false;
                 });
-            Condition travelSelected = new Condition("Travel has begun",
+            Condition travelSelected = new Condition("travelSelected", "Travel has begun",
                 null,
                 (selection, world) =>
                 {
                     return world.IsCurrentAction("beginTravel");
                 });
-            Condition canTravel = new Condition("The player can travel: all must be true", 
-                andOperator, 
+            Condition worldStateGood = new Condition("goodWorldState", "I can check the last action",
+                null,
+                (selection, world) =>
+                {
+                    return world.CurrentAction.Description(world).Contains("butts"); // this is weird. 
+                });
+            Condition canTravel = new Condition("travelOperator", "The player can travel: all must be true",
+                andOperator,
                 new List<Condition>(){
                 atRest, validDestination, notTravelling, travelSelected
             });
